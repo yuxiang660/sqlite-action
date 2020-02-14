f87a34c4e88d990191b579c5d7b4bba737c8f635
yuxiang660 <yuxiang660@163.com>
2020-02-15 02:22:41 +0800

    Setup server test env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -l
 
-apt-get update && apt-get install -y sqlite3
-sqlite3 --version
+apt-get update && apt-get install -y software-properties-common && add-apt-repository ppa:longsleep/golang-backports && apt install -y golang-go && apt install -y redis-server && apt-get install -y sqlite3
 
 echo "Hello $1"
 time=$(date)
